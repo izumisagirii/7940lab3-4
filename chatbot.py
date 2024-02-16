e1247fb8c2ecf864840112f425b5cd861029fac2
Yongheng CUI <cuiyongheng1@outlook.com>
2024-02-17 05:21:21 +0800

    write ups

--- chatbot.py
+++ chatbot.py
@@ -29,6 +29,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("hello", hello_command))
 
     # register a dispatcher to handle message: here we register an echo dispatcher
     # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
@@ -51,14 +52,6 @@ def echo(update, context):
     logging.info("context: " + str(context))
     context.bot.send_message(
         chat_id=update.effective_chat.id, text=reply_message)
-
-
-def echo(update, context):
-    reply_message = update.message.text.upper()
-    logging.info("Update: " + str(update))
-    logging.info("context: " + str(context))
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text=reply_message)
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 
@@ -66,6 +59,16 @@ def echo(update, context):
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Helping you helping you.')
+
+
+def hello_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /hello is issued."""
+    try:
+        logging.info(context.args[0])
+        msg = context.args[0]  # /add keyword <-- this should store the keyword
+        update.message.reply_text('Good day ' + msg + '!')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /add <keyword>')
 
 
 def add(update: Update, context: CallbackContext) -> None:
